e6ea979b2a29659b3e0e4049ddf32abd7b6eade6
amrutha sireesh <amrutha.sireesh@co-cc02846.ad.cabinetoffice.gov.uk>
2020-11-09 09:32:29 +0000

    remove report plugin

--- src/test/java/com/scale/TestRunner/TestRunner.java
+++ src/test/java/com/scale/TestRunner/TestRunner.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
         features = {"src/test/resources/FeatureFiles"},
         glue={"com.scale.stepdefs", "com.scale.context"},
         monochrome = true,
-        plugin = { "pretty", "html:target/cucumber-html-reports", "json:target/cucumber.json"},
+       // plugin = { "pretty", "html:target/cucumber-html-reports", "json:target/cucumber.json"},
+       // plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         tags = "@regTests or @Prod or @QnErrTests or @fm2-housing or @fm2-security-sw " +
                 "or @fm2-tech-security or @legal or @linen or @techEdu or @errhandling",
         dryRun = false
